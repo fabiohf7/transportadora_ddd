d6e344630d5c22ef894b158f1d6a7a5657fdbe54
Fábio Henrique Ffaonfaseca <fabiohf7@gmafail.com>
2020-05-13 14:11:02 -0300

    adicionando autofac no projeto

--- TransportadoraFabriq.MVC/Startup.cs
+++ TransportadoraFabriq.MVC/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -11,6 +13,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TransportadoraFabriq.Infra.Data.Context;
+using TransportadoraFabriq.MVC.Config.IoC;
 
 namespace TransportadoraFabriq.MVC
 {
@@ -24,18 +27,28 @@ namespace TransportadoraFabriq.MVC
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
+            });            
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddCustomDbContext(Configuration);
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //Autofac
+            var builder = new ContainerBuilder();
+
+            builder.Populate(services);
+            builder.RegisterModule(new ApplicationModule());
+
+            var container = builder.Build();
+
+            return new AutofacServiceProvider(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
