77506765d077eb9aec38305684441b2a818cdc37
Fábio Henrique Ffaonfaseca <fabiohf7@gmail.com>
2020-05-18 20:34:54 -0300

    criacao do command/handler de itinerario

---
+++ TransportadoraFabriq.Application/Transporte/Command/AdicionarItinerarioCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Runtime.Serialization;
+using TransportadoraFabriq.Domain.Transporte;
+using TransportadoraFabriq.Shared.Commands.Interfaces;
+
+namespace TransportadoraFabriq.Application.Transporte.Command
+{
+    [DataContract]
+    public class AdicionarItinerarioCommand : ICommand
+    {
+        [DataMember]
+        public Motorista Motorista { get; private set; }
+
+        public Veiculo Veiculo { get; private set; }
+
+        public List<Entrega> Entregas { get; private set; }
+
+        public AdicionarItinerarioCommand(Motorista motorista, Veiculo veiculo, List<Entrega> entregas)
+        {
+            Motorista = motorista;
+            Veiculo = veiculo;
+            Entregas = entregas;
+        }
+    }
+}
---
+++ TransportadoraFabriq.Application/Transporte/CommandHandler/AdicionarVeiculoCommandHandler.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TransportadoraFabriq.Application.Transporte.Command;
+using TransportadoraFabriq.Domain.Transporte.Repositories;
+using TransportadoraFabriq.Shared.Commands;
+using TransportadoraFabriq.Shared.Entities.Interfaces;
+using TransportadoraFabriq.Shared.Notification;
+
+namespace TransportadoraFabriq.Application.Transporte
+{
+    public class AdicionarItinerarioCommandHandler : CommandHandler, IRequestHandler<AdicionarItinerarioCommand, CommandResult>
+    {
+        private readonly IItinerarioRepository _itinerarioRepository;
+
+        public AdicionarItinerarioCommandHandler(IItinerarioRepository itinerarioRepository, IUnitOfWork uow, IMediator mediator, INotificationHandler<NotificationDomain> notifications, ILogger<CommandHandler> logger = null) : base(uow, mediator, notifications, logger)
+        {
+            _itinerarioRepository = itinerarioRepository;
+        }
+
+        public Task<CommandResult> Handle(AdicionarItinerarioCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
